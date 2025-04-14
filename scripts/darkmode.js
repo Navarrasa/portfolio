1d0804ff555d88fa4b7e227c2221bc3ecf364cae
Gustavo <gugubrunopf@gmail.com>
2025-04-14 20:28:30 -0300

    portfolio finished!

--- scripts/darkmode.js
+++ scripts/darkmode.js
@@ -1,12 +1,19 @@
 const moonIcon = document.getElementById('moon');
 const sunIcon = document.getElementById('sun');
 let lightmode = localStorage.getItem('lightmode');
+const VIDEO_CLARO_URL = "https://github.com/Navarrasa/ferramentas/raw/refs/heads/main/videos/portfolio_lightmode.mp4";
+const VIDEO_ESCURO_URL = "https://github.com/Navarrasa/ferramentas/raw/refs/heads/main/videos/portfolio_darkmode.mp4";
+const videoPlayer = document.getElementById('videoPlayer');
 
 const enableLightmode = () => {
     document.body.classList.add('lightmode');
     localStorage.setItem('lightmode', 'active');
     moonIcon.classList.remove('active');
     sunIcon.classList.add('active');
+    // Atualiza o vídeo para o modo claro
+    videoPlayer.src = VIDEO_CLARO_URL;
+    // Inicia o vídeo (opcional, dependendo do seu comportamento desejado)
+    videoPlayer.play();
 };
 
 const disableLightmode = () => {
@@ -14,26 +21,17 @@ const disableLightmode = () => {
     localStorage.setItem('lightmode', null);
     moonIcon.classList.add('active');
     sunIcon.classList.remove('active');
+    // Atualiza o vídeo para o modo escuro
+    videoPlayer.src = VIDEO_ESCURO_URL;
+    // Inicia o vídeo (opcional, dependendo do seu comportamento desejado)
+    videoPlayer.play();
 };
 
-const disableIcon = () => {
-    if (lightmode === 'active') {
-        moonIcon.classList.remove('active');
-        sunIcon.classList.add('active');
-    }
-    else {
-        moonIcon.classList.add('active');
-        sunIcon.classList.remove('active');
-    }
-}
-
-// Inicializa o tema com base no localStorage
+// Inicializa o tema e o vídeo com base no localStorage
 if (lightmode === 'active') {
     enableLightmode();
-    disableIcon();
 } else {
     disableLightmode();
-    disableIcon();
 }
 
 // Adiciona eventos de clique aos ícones
