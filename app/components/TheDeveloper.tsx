ccde93828800d9da9e5d2d275993a867b553d1d9
Gustavo Bruno <gugubrunopf@gmail.com>
2025-09-16 22:47:05 -0300

    feat: developing tech stack

--- app/components/TheDeveloper.tsx
+++ app/components/TheDeveloper.tsx
@@ -1,13 +1,23 @@
+import { tech_stack } from "@/app/data/data";
+import Image from "next/image";
+
 export default function TheDeveloper() {
     return (
-        <section className="w-full h-full flex flex-col items-center justify-center">
+        <section className="w-full h-full flex flex-col items-center justify-center p-4">
             <div className="flex flex-col gap-4 max-w-7xl">
                 <div className="w-full">
-                    <h1 className='rubik text-[8rem]'>Backend Developer</h1>
+                    <h1 className='rubik text-[7rem]'>Backend Developer</h1>
                 </div>
                 <div className="flex gap-16">
                     <p className="w-5/12">I am a backend developer with a passion for building scalable APIs and working with databases.</p>
                     <p className="w-96">My tech stack includes Python, Django, and PostgreSQL.</p>
+                </div>
+                <div>
+                    {tech_stack.map((tech) => (
+                        <div key={tech.label} className="flex items-center gap-2">
+                            <Image src={tech.image} alt={tech.label} width={40} height={40} />
+                        </div>
+                    ))}
                 </div>
             </div>
         </section>
---
+++ app/data/data.ts
@@ -0,0 +1,68 @@
+import Python from '/python.svg';
+import Django from '/django.svg';
+import Git from '/git.svg';
+import Java from '/java.svg';
+import Javascript from '/javascript.svg';
+import Typescript from '/typescript.svg';
+import MySQL from '/mysql.svg';
+import NextJS from '/nextjs.svg';
+import React from '/react.svg';
+import Tailwind from '/tailwindcss.svg';
+import FastAPI from '/fastapi.svg';
+import Vite from '/vitejs.svg';
+import SQLite from '/sqlite.svg';
+
+export const tech_stack = [
+    {
+        image: Python,
+        label: 'Python',
+    },
+    {
+        image: Django,
+        label: 'Django',
+    },
+    {
+        image: FastAPI,
+        label: 'FastAPI',
+    },
+    {
+        image: Java,
+        label: 'Java',
+    },
+    {
+        image: Javascript,
+        label: 'Javascript',
+    },
+    {
+        image: Typescript,
+        label: 'Typescript',
+    },
+    {
+        image: MySQL,
+        label: 'MySQL',
+    },
+    {
+        image: SQLite,
+        label: 'SQLite',
+    },
+    {
+        image: NextJS,
+        label: 'NextJS',
+    },
+    {
+        image: React,
+        label: 'React',
+    },
+    {
+        image: Tailwind,
+        label: 'TailwindCSS',
+    },
+    {
+        image: Vite,
+        label: 'ViteJS',
+    },
+    {
+        image: Git,
+        label: 'Git',
+    },  
+];
